0d251e594cd8b5af26aed6f61eedcb9985ed96a3
Toru KAWAMURA <tkawa@4bit.net>
2024-09-08 14:41:09 +0900

    allow variant routing

--- lib/actionpack/action_caching/railtie.rb
+++ lib/actionpack/action_caching/railtie.rb
@@ -11,6 +11,28 @@ module ActionPack
           require "action_view/rendering_caching"
           ActionView::TemplateRenderer.send :prepend, ActionView::RenderingCaching
         end
+        ActiveSupport.on_load(:after_initialize) do
+          ActionDispatch::Routing::Mapper::Mapping.class_eval do
+            def self.normalize_path(path, format)
+              path = ActionDispatch::Routing::Mapper.normalize_path(path)
+
+              if format == true
+                "#{path}.:format"
+              elsif optional_format_with_variant?(path, format)
+                "#{path}(+:variant)(.:format)"
+              elsif optional_format?(path, format)
+                "#{path}(.:format)"
+              else
+                path
+              end
+            end
+
+            def self.optional_format_with_variant?(path, format)
+              format == :with_variant && optional_format?(path, format)
+            end
+          end
+          ActionDispatch::Routing::Mapper::Mapping::JOINED_SEPARATORS << '+'
+        end
       end
     end
   end
